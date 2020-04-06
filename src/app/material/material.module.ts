2048b94dd6c957a64891ee3519c40c0d3f390f78
Timotej Zaťko <timi.zatko@gmail.com>
2020-04-06 14:46:54 +0200

    Add angular material framework

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
+
+@NgModule({
+    imports: [CommonModule, MatSliderModule],
+    exports: [MatSliderModule],
+})
+export class MaterialModule {}
